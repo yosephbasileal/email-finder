d4bbc78f12cdd51b18cf883e75dd66c702c5fcc3
Basileal Imana <yosephbasileal@yahoo.com>
2016-02-22 21:47:59 -0500

    add command line options and fix max-depth constraint

--- email-finder.py
+++ email-finder.py
@@ -2,6 +2,7 @@ import re, urllib
 from bs4 import BeautifulSoup
 import urlparse
 import Queue
+import argparse
 
 # Attemps to connect to a webpage and download html
 def get_file(url):
@@ -21,10 +22,11 @@ def get_emails(html):
 
 # Gets all links in an html string that link to a page within the given domain
 def get_links(html, base_url):
-	links = set()
+	links = set() # use a set to avoid duplicates
 	s = BeautifulSoup(html, "html.parser")
 	a = s.find_all('a')
-	# for each 'a' element found
+
+	# for each 'a' html element found
 	for link in a:
 		# get link address
 		l = link.get('href')
@@ -33,7 +35,7 @@ def get_links(html, base_url):
 		new_url = urlparse.urljoin(base_url, l)
 
 		# parse the full url
-		o = urlparse.urlparse(new_url)
+		o = urlparse.urlsplit(new_url)
 
 		# parse base_url to get domain
 		d = urlparse.urlparse(base_url)
@@ -41,13 +43,29 @@ def get_links(html, base_url):
 		# add only if from the same domain
 		if(o.netloc == d.netloc):
 			links.add(o.geturl())
+
 	return links
+
+# Parses command line arguments
+def arg_parser():
+	parser = argparse.ArgumentParser(
+		description="Crawls a webstie and look for email addresses")
+	parser.add_argument('domain_name', 
+		help = "Website to be crawled")
+	parser.add_argument('-d',
+		help = "Maximum page tree depth to reach",action="store", default=1)
+	parser.add_argument('-o',
+		help = "Output url being crawled for debugging purpose", action="store_true", default=False)
+	return parser.parse_args()
+
 
 # Main runner function
 def run():
-	# get url
-	base_url = "http://www.trincoll.edu/"
-	depth_max = 4
+	# get arguments
+	args = arg_parser()
+	base_url = "http://" + args.domain_name
+	depth_max = args.d
+	output = args.o
 
 	# initializes queue and set of emails and links
 	q = Queue.Queue()
@@ -55,17 +73,22 @@ def run():
 	all_emails = set()
 
 	# start crawling from base_url
-	q.put(base_url)
+	q.put((base_url, 0)) # queue stores a (link, depth) tuple
 	all_links.add(base_url)
-
-	# keeps track of depth of page-tree
-	depth = 1;
 	
 	# while there are remaining pages to crawl
 	while not q.empty():
+
+		# pop next link from queue
+		url_t = q.get() # get (link, depth) tuple
+		url = url_t[0] # get url
+		depth = url_t[1] # get current depth
+
+		# output for debugging
+		if(output):
+			print "(" + str(depth) + ")" + url
+
 		# get html string of current page
-		url = q.get()
-		print url
 		html = get_file(url)
 		
 		# extract emails addresses
@@ -77,16 +100,13 @@ def run():
 		# for each link found
 		for link in links:
 			# check if not already visited and if depth doesnt exceed max-depth
-			if link not in all_links and depth < depth_max:
-				# add to queue
-				q.put(link)
+			if link not in all_links and depth <= depth_max:
+				# add to queue, increment depth
+				q.put((link, depth + 1))
 
 		# update set
 		all_emails.update(emails)
 		all_links.update(links)
-
-		# increment depth
-		depth = depth + 1
 
 	# output emails, if any
 	for e in all_emails:
